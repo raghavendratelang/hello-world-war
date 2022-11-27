feefd953723d6d323d3c01a053b45be3ae3a61a2
prajwal1327 <118236029+prajwal1327@users.noreply.github.com>
2022-11-27 23:53:01 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+apt install maven
+#delete the existing folder/code to execute the fresh command
+rm -rf hello-world-war
+#need to clone first
+git clone https://github.com/prajwal1327/hello-world-war.git
+#switch to hello-world-war
+cd  hello-world-war
+#run the maven package
+mvn package
+cp -R target/hello-world-war-1.0.0.war Tomcatinstallation/tomcat/webapps/
+#stop and start the tomcat
+sh Tomcatintallation/tomcat/bin/shutdown.sh 
+sleep 2
+sh Tomcatintallation/tomcat/bin/shartup.sh
