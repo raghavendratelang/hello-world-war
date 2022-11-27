7981aecab480846bf7cbaeb3cafb8cd21435d5c4
prajwal1327 <118236029+prajwal1327@users.noreply.github.com>
2022-11-28 00:02:35 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-sudo apt update
-sudo apt install maven
+apt update
+apt install maven
 #delete the existing folder/code to execute the fresh command
 rm -rf hello-world-war
 #need to clone first
 git clone https://github.com/prajwal1327/hello-world-war.git
 #switch to hello-world-war
-cd  hello-world-war
+cd hello-world-war
 #run the maven package
 mvn package
-cp -R target/hello-world-war-1.0.0.war Tomcatinstallation/tomcat/webapps/
+cp -R Tomcatinstallation/tomcat/target/hello-world-war-1.0.0.war Tomcatinstallation/tomcat/webapps/
 #stop and start the tomcat
-sh workspace/Tomcatinstallation/tomcat/bin/shutdown.sh
+sh Tomcatinstallation/tomcat/bin/shutdown.sh
 sleep 2
-sh workspace/Tomcatinstallation/tomcat/bin/startup.sh
+sh Tomcatinstallation/tomcat/bin/startup.sh
